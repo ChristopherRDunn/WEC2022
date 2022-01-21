63867ee50f3fbe4fff552eddd29bda63cc336f73
Serena-Tang <83049700+Serena-Tang@users.noreply.github.com>
2022-01-21 15:49:39 -0500

    Create form.js

---
+++ src/form.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default class form extends React.Component {
+  state = {
+    inputConfig: "",
+    processConfig: "",
+  };
+
+  change = e => {
+    this.props.onChange({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    // this.props.onSubmit(this.state);
+    this.setState({
+      inputConfig: "",
+      processConfig: "",
+    });
+    this.props.onChange({
+      inputConfig: "",
+      processConfig: "",
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          name="inputConfig"
+          placeholder="Input Configuration"
+          value={this.state.inputConfig}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <input
+          name="processConfig"
+          placeholder="Process Configuration"
+          value={this.state.processConfig}
+          onChange={e => this.change(e)}
+        />
+        <br />
+        <button onClick={e => this.onSubmit(e)}>Submit</button>
+      </form>
+    );
+  }
+}
