41ac4fed51c000137b65c94e09614b5d91d2cb63
Serena-Tang <83049700+Serena-Tang@users.noreply.github.com>
2022-01-21 15:39:29 -0500

    import form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Form from "./form";
 
-function App() {
+
+class App extends Component {
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
+  render() {  
   return (
     <div className="App">
       <header className="App-header">
+        <Form onChange={fields => this.onChange(fields)} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          {JSON.stringify(this.state.fields, null, 2)}
         <a
           className="App-link"
           href="https://reactjs.org"
